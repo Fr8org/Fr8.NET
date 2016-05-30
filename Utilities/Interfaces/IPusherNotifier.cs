adb713c3fba03ce732dcf24fb535a49905fa622d
Bahadır Bozdağ <bahadir.bb@gmail.com>
2016-05-30 10:47:41 +0300

    Initial migration of core projects

---
+++ Utilities/Interfaces/IPusherNotifier.cs
@@ -0,0 +1,8 @@
+﻿namespace Utilities.Interfaces
+{
+	public interface IPusherNotifier
+	{
+		void Notify(string channelName, string eventName, object message);
+        void NotifyUser(object message, string eventName, string userName);
+	}
+}
